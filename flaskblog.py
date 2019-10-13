cd89660f5489cd6e8af0454e5527a0c0528b8c36
fonkamloic <fonkamloic@gmail.com>
2019-10-13 04:52:06 +0200

    Update to project

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , url_for, flash
+from flask import Flask, render_template , url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -33,11 +33,18 @@ def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
 	return render_template('register.html', title="Register", form=form) 
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'fonkamloic@gmail.com' and form.password.data == 'Passw0rd':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unseccessflu. Please check username and password', 'danger')
 	return render_template('login.html', title="Login", form=form) 
 
 
